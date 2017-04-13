6a648989d0ff1a5fe5a78ecdb76b7039f17f846e
danielyaakove <danielyaakove@gmail.com>
2017-04-14 01:21:26 +0300

    vv

---
+++ src/app/test.ts
@@ -0,0 +1,58 @@
+class MenuItem {
+  // Our properties:
+  // By default they are public, but can also be private or protected.
+  items: Array<string>;  // The items in the menu, an array of strings.
+  pages: number;         // How many pages will the menu be, a number.
+
+
+  // A straightforward constructor. 
+  constructor(item_list: Array<string>, total_pages: number) {
+    // The this keyword is mandatory.
+    this.items = item_list;    
+    this.pages = total_pages;
+  }
+
+  // Methods
+  list(): void {
+    console.log("Our menu for today:");
+    for(var i=0; i<this.items.length; i++) {
+      console.log(this.items[i]);
+    }
+  }
+  getName():string{
+      return 'sss';
+  }
+
+} 
+
+
+
+// Create a new instance of the Menu class.
+var sundayMenu = new MenuItem(["pancakes","waffles","orange juice"], 1);
+
+var i:string;
+
+i = sundayMenu.getName();
+// Call the list method.
+sundayMenu.list();
+
+
+// The <T> after the function name symbolizes that it's a generic function.
+// When we call the function, every instance of T will be replaced with the actual provided type.
+
+// Receives one argument of type T,
+// Returns an array of type T.
+
+function genericFunc<T>(argument: T): T[] {    
+  var arrayOfT: T[] = [];    // Create empty array of type T.
+  arrayOfT.push(argument);   // Push, now arrayOfT = [argument].
+  return arrayOfT;
+}
+
+var arrayFromString = genericFunc<string>("beep");
+console.log(arrayFromString[0]);         // "beep"
+console.log(typeof arrayFromString[0])   // String
+
+var arrayFromNumber = genericFunc(42);
+console.log(arrayFromNumber[0]);         // 42
+console.log(typeof arrayFromNumber[0])   // number
